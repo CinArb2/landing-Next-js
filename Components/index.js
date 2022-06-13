a1884b9a2548d323a543706011cf5c1c1272c239
CinArb2 <91969387+CinArb2@users.noreply.github.com>
2022-06-13 06:49:42 -0500

    fix relative path

--- Components/index.js
+++ Components/index.js
@@ -1,7 +1,7 @@
-import Button from './Button'
-import Video from './Video'
-import Card from './Card'
-import CardServices from './CardServices'
-import FloatingBtn from './FloatingBtn'
+import Button from '/Button'
+import Video from '/Video'
+import Card from '/Card'
+import CardServices from '/CardServices'
+import FloatingBtn from '/FloatingBtn'
 
 export { Button, Video, Card, CardServices, FloatingBtn}
