35447e85fd993bbee49404685ae44056278eddd4
cindyv-ciandt <cindyv@ciandt.com>
2023-03-18 14:45:48 -0500

    fix: enable avif support on next config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
 }
 
+// eslint-disable-next-line no-undef
 module.exports = nextConfig
